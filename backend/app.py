5795fe40ac9be908605c6310a02f832ab581f7b7
Juan Erquiaga <erquiagajuan98@gmail.com>
2025-12-28 16:03:21 -0500

    fixing cors stuff

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://*.vercel.app",
-        "https://plinkitforme.vercel.app",
+        "https://plink-it-for-me.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
